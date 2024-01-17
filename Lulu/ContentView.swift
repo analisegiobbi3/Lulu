9eb7e74fd91ec3a4c093c98c8e382a1ece5814f3
analisegiobbi3 <59839160+analisegiobbi3@users.noreply.github.com>
2024-01-17 17:13:58 -0500

    Init

--- Lulu/ContentView.swift
+++ Lulu/ContentView.swift
@@ -6,18 +6,44 @@
 //
 
 import SwiftUI
+import AVFoundation
 
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            ZStack {
+                Color(red: 238, green: 0.55, blue: 0.66).edgesIgnoringSafeArea(.all)
+                VStack {
+                    Text("Lulu")
+                        .font(Font.custom("Borel-Regular", size: 40))
+                        .bold()
+                        .foregroundColor(.white)
+                    Button(action: {treatSound(soundName: "treats")}, label: {
+                        Image("luluPaw")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                    })
+                    Text("Treat Now!")
+                        .font(Font.custom("Borel-Regular", size: 40))
+                        .bold()
+                        .foregroundColor(.white)
+                }
+                .padding(.all)
+            }
+            
         }
-        .padding()
+        
     }
 }
+
+func treatSound(soundName: String) {
+    var player: AVAudioPlayer!
+    let url = Bundle.main.url(forResource: soundName, withExtension: "m4a")
+    player = try! AVAudioPlayer(contentsOf: url!)
+    player.play()
+}
+
+
 
 #Preview {
     ContentView()
---
+++ README.md
@@ -0,0 +1,27 @@
+//
+//  README.md
+//  Lulu
+//
+//  Created by A Giobbi on 1/17/24.
+//
+
+# Lulu
+
+## An app specifically for Lulu so that she can have as many treats as she wants
+
+### This app is to practice simple SwiftUI and create a silly little app for Lulu so she can request treats at the tap of a button 
+
+## Purpose
+
+No real purpose other than playing around with swiftUI for fun
+
+## App 
+
+![Screenshot of the Lulu App](/var/folders/p5/ltw2zzfs55j83frcp41fzymh0000gp/T/TemporaryItems/NSIRD_screencaptureui_4s3mS1/Screenshot 2024-01-17 at 5.08.51 PM.png)
+
+## Usage
+
+Tap the button and a voice says treat. I actually got my cat to come running when the audio played. 
+
+
+
