cc8785217a24429832e26c1242d7f2f60c5e95d2
analisegiobbi3 <59839160+analisegiobbi3@users.noreply.github.com>
2024-01-17 16:27:40 -0500

    Initial Commit

---
+++ Lulu/LuluApp.swift
@@ -0,0 +1,17 @@
+//
+//  LuluApp.swift
+//  Lulu
+//
+//  Created by A Giobbi on 1/17/24.
+//
+
+import SwiftUI
+
+@main
+struct LuluApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
